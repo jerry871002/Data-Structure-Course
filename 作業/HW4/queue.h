c81d5073e2ffe2d6cd6e841ac015edb5c97d8f54
jerry <jerry871002@gmail.com>
2018-10-26 18:31:45 +0800

    HW4來了QQ

---
+++ 作業/HW4/queue.h
@@ -0,0 +1,29 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdbool.h>
+
+typedef struct node {
+    void* dataPtr;
+    struct node* next;
+} QUEUE_NODE;
+
+typedef struct {
+    QUEUE_NODE* front;
+    QUEUE_NODE* rear;
+    int count;
+} QUEUE;
+
+QUEUE* createQueue(void);
+QUEUE* destroyQueue(QUEUE* queue);
+
+bool dequeue(QUEUE* queue, void** itemPtr);
+bool enqueue(QUEUE* queue, void* itemPtr);
+bool queueFront(QUEUE* queue, void** itemPtr);
+bool queueRear(QUEUE* queue, void** itemPtr);
+int queueCount(QUEUE* queue);
+
+bool emptyQUeue(QUEUE* queue);
+bool fullQueue(QUEUE* queue);
+
+#endif
