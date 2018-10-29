5bf5f914df614f4b51a84a57b0db9f9f7afbe2df
jerry <jerry871002@gmail.com>
2018-10-29 12:06:53 +0800

    HW4程式題第二題努力中

--- 作業/HW4/hw4_22.c
+++ 作業/HW4/hw4_22.c
@@ -1,6 +1,58 @@
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+typedef struct {
+    int custNum;
+    int arriveTime;
+} custData;
+
+typedef struct {
+    int custNum;
+    int arriveTime;
+    int startTime;
+    int svcTime;
+} custStatus;
+
+typedef struct {
+    int numCust;
+    int totSvcTime;
+    int totWaitTime;
+    int maxQueueSize;
+} simStats;
 
 int main(int argc, char const *argv[]) {
-    /* code */
+    srand(time(NULL));
+    QUEUE *queue = createQueue();
+    bool moreCusts;
+
+    custStatus* status = (custStatus*)malloc(sizeof(custStatus));
+    simStats* stats = (simStats*)malloc(sizeof(simStats));
+    stats->numCust = stats-> totSvcTime = stats->totWaitTime = stats->maxQueueSize = 0;
+
+    int clock, serviceProvided, maxServiceTime;
+    printf("Please enter the length of time (in hour) that the service is provided: ");
+    scanf("%d\n", &serviceProvided);
+    printf("Please enter the maximum service time (in minute): ");
+    scanf("%d\n", &maxServiceTime);
+
+    int* custNum = (int*)malloc(sizeof(int));
+    *custNum = 0
+    for(clock = 1; clock <= serviceProvided * 60 || moreCusts; currentClock++) {
+        newCustomer(queue, clock, custNum);
+    }
+
     return 0;
+}
+
+newCustomer(QUEUE* queue, int clock, int* custNum) {
+    int randomNum = rand() % 9;
+    if(randomNum > 4) {
+        custNum++;
+        custData* cust = (custData*)malloc(sizeof(custData));
+        cust->custNum = *custNum;
+        cust->arriveTime = clock;
+        enqueue(queue, cust);
+    }
 }
---
+++ 作業/HW4/做成交作業的格式/0610780_4_20.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+bool isNum(void* data);
+
+int main(int argc, char const *argv[]) {
+    FILE* inFile = fopen("test.txt", "r");
+    QUEUE* queue = createQueue();
+
+    char data;
+    while(fscanf(inFile, "%c", &data) != EOF) {
+        printf("Data read: %c\n", data);
+        if(data == '+' || data == '-' || data == '*' || data == '/') {
+            char* dataPtr = (char*)malloc(sizeof(char));
+            *dataPtr = data;
+            enqueue(queue, dataPtr);
+        } else if(data >= '0' && data <= '9') {
+            int* dataPtr = (int*)malloc(sizeof(int));
+            *dataPtr = (int)data - 48;
+            enqueue(queue, dataPtr);
+        }
+    }
+
+    void *item;
+    char *operator;
+    int *operandOne, *operandTwo;
+    while(queueCount(queue) != 1) {
+        dequeue(queue, &item);
+        operator = (char*)item;
+
+        queueFront(queue, &item);
+        if(isNum(item)) {
+            dequeue(queue, &item);
+            operandOne = (int*)item;
+
+            queueFront(queue, &item);
+            if(isNum(item)) {
+                dequeue(queue, &item);
+                operandTwo = (int*)item;
+
+                int *result = (int*)malloc(sizeof(int));
+                switch (*operator) {
+                    case '+':
+                        *result = (*operandOne) + (*operandTwo);
+                        break;
+                    case '-':
+                        *result = (*operandOne) - (*operandTwo);
+                        break;
+                    case '*':
+                        *result = (*operandOne) * (*operandTwo);
+                        break;
+                    case '/':
+                        *result = (*operandOne) / (*operandTwo);
+                        break;
+                }
+
+                enqueue(queue, result);
+                
+                free(operator);
+                free(operandOne);
+                free(operandTwo);
+            } else {
+                enqueue(queue, operator);
+                enqueue(queue, operandOne);
+            }
+        } else {
+            enqueue(queue, operator);
+        }
+
+    }
+    int* finalResult;
+    dequeue(queue, &finalResult);
+    printf("The answer is %d.\n", *finalResult);
+
+    free(finalResult);
+    destroyQueue(queue);
+
+    return 0;
+}
+
+bool isNum(void* data) {
+    char* dataChar = (char*)data;
+    return (*dataChar != '+' && *dataChar != '-' && *dataChar != '*' && *dataChar != '/');
+}
