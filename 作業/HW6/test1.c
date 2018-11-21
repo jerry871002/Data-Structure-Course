55533c2088095acb7e48f87a68e4e66409c9d035
jerry <jerry871002@gmail.com>
2018-11-21 12:12:03 +0800

    HW6努力中

---
+++ 作業/HW6/test1.c
@@ -0,0 +1,109 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <limits.h>
+
+#define SIZE 26
+
+typedef struct node {
+    bool isLeaf;
+    int value;
+    char letter;
+    struct node *left, *right;
+} NODE;
+
+// According to Table 6-3
+int letterValue[26] = {7, 2, 2, 3, 11, 2, 2, 6, 6, 1, 1, 4, 3, 7, 9, 2, 1, 6, 6, 8, 4, 1, 2, 1, 2, 1};
+
+NODE* createHuffmanTree();
+makeHuffmanCode(NODE* huffmanTree, char* huffmanCode[]);
+bool checkComplete(NODE* array[], int size);
+
+int main(int argc, char const *argv[]) {
+
+    NODE* huffmanTree = createHuffmanTree();
+    char* huffmanCode[26]
+    makeHuffmanCode(huffmanTree, huffmanCode);
+
+    printf("Options:\n");
+    printf("(1)String to Huffman Code.\n");
+    printf("(2)Huffman Code to String.\n");
+    printf("Please choose the function you need: ");
+
+    int option;
+    scanf("%d", &option);
+
+    switch (option) {
+        case 1:
+            break;
+        case 2:
+            break;
+        default:
+            break;
+    }
+
+    return 0;
+}
+
+NODE* createHuffmanTree() {
+    NODE* array[SIZE];
+    for(int i = 0; i < SIZE; i++) {
+        array[i] = (NODE*)malloc(sizeof(NODE));
+        array[i]->isLeaf = true;
+        array[i]->value = letterValue[i];
+        array[i]->letter = (char)(65 + i);
+        array[i]->left = NULL;
+        array[i]->right = NULL;
+    }
+
+    while(!checkComplete(array, SIZE)) {
+        NODE initial;
+        initial.value = INT_MAX;
+        NODE* min1 = &initial;
+        NODE* min2 = &initial;
+        int min1index, min2index;
+        for(int i = 0; i < SIZE; i++) {
+            if(array[i]) {
+                if(array[i]->value < min1->value) {
+                    min2 = min1;
+                    min2index = min1index;
+                    min1 = array[i];
+                    min1index = i;
+                } else if(array[i]->value <= min2->value) {
+                    min2 = array[i];
+                    min2index = i;
+                }
+            }
+        }
+
+        NODE *newNode = (NODE*)malloc(sizeof(NODE));
+        newNode->isLeaf = false;
+        newNode->value = min1->value + min2->value;
+        newNode->left = min1;
+        newNode->right = min2;
+
+        array[min1index] = newNode;
+        array[min2index] = NULL;
+    }
+
+    for(int i = 0; i < SIZE; i++) {
+        if(array[i])
+            return array[i];
+    }
+}
+
+makeHuffmanCode(NODE* huffmanTree, char* huffmanCode[]) {
+
+}
+
+bool checkComplete(NODE* array[], int size) {
+    int count = 0;
+    for(int i = 0; i < size; i++) {
+        if(array[i])
+            count++;
+        if(count > 1)
+            return false;
+    }
+
+    return true;
+}
