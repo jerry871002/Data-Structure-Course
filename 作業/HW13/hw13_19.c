e3a32e533a78b525f0e2cd98e84d7bea7810fb0c
jerry <jerry871002@gmail.com>
2019-01-04 18:11:17 +0800

    HW13最後努力中

--- 作業/HW13/hw13_19.c
+++ 作業/HW13/hw13_19.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "list.h"
 
 #define TABLE_SIZE 4
@@ -18,10 +19,73 @@ int main() {
 
     FILE* inFile = fopen("inventory.txt", "r");
 
-    int partNum, quantity;
-    while(fscanf("%d %d", &partNum, &quantity) != EOF) {
+    int partNum, quantity, totalNode = 0;
+    while(fscanf(inFile, "%d %d", &partNum, &quantity) != EOF) {
+        totalNode++;
+
         INVENTORY* newPart = (INVENTORY*)malloc(sizeof(INVENTORY));
+        newPart->partNum = partNum;
+        newPart->quantity = quantity;
+
+        addNode(hashList[partNum%TABLE_SIZE], newPart);
     }
+
+    fclose(inFile);
+
+    // Show all content in the hash table
+    printf("Home Addr\tPrime Area\tOverflow List\n");
+    for(int i = 0; i < TABLE_SIZE; i++) {
+        // Print home address
+        printf("%9d\t", i);
+
+        if(!emptyList(hashList[i])) {
+            // Print prime area
+            printf("%5d/%-4d\t", ((INVENTORY*)(hashList[i]->head->dataPtr))->partNum, ((INVENTORY*)(hashList[i]->head->dataPtr))->quantity);
+
+            // If there are overflow list
+            if(hashList[i]->count > 1) {
+                NODE* walk = hashList[i]->head->link;
+                for(int j = 0; j < (hashList[i]->count) - 1; j++) {
+                    printf("%d/%d ", ((INVENTORY*)(walk->dataPtr))->partNum, ((INVENTORY*)(walk->dataPtr))->quantity);
+                    walk = walk->link;
+                }
+            }
+            printf("\n");
+        }
+    }
+    printf("\n");
+
+    int primeCount = 0, listCount = 0, longest = 0;
+    for(int i = 0; i < TABLE_SIZE; i++) {
+        if(!emptyList(hashList[i])) {
+            primeCount++;
+            if(hashList[i]->count > 1)
+                listCount ++;
+            if(hashList[i]->count > longest)
+                longest = hashList[i]->count;
+        }
+    }
+
+    printf("Percentage of prime area filled: %5.1f %%\n", (float)primeCount/TABLE_SIZE*100);
+    printf("Average nodes in linked list:    %5.1f node(s)\n", (float)(totalNode-primeCount)/listCount);
+    printf("Logest linked list:              %5d node(s)\n", longest-1);
+
+    int seachNum;
+    printf("Please enter a part number to search: ");
+    scanf("%d", &searchNum);
+
+    bool found = false;
+    for(int i = 0; i < TABLE_SIZE; i++) {
+        if(!emptyList(hashList[i])) {
+            NODE* walk = hashList[i]->head;
+            for(int j = 0; j < hashList[i]->count; j++) {
+
+            }
+        }
+    }
+
+    printf("\n");
+
     return 0;
 }
 
