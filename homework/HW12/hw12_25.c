dafb3de610d180c1f9af87e98df3e298f6af4695
Jerry Yang <jerry871002@gmail.com>
2023-07-13 11:08:31 +0000

    HW12 cleanup

--- homework/HW12/hw12_25.c
+++ homework/HW12/hw12_25.c
@@ -29,53 +29,31 @@ int main() {
     return 0;
 }
 
-
-
-// Merges two subarrays of arr[]
-// First subarray is arr[left...mid]
-// Second subarray is arr[mid+1...right]
 void merge(int arr[], int left, int mid, int right) {
     int i, j, k;
     int leftCount = mid - left + 1;
     int rightCount = right - mid;
 
-    // create temp arrays
     int L[leftCount], R[rightCount];
-
-    // Copy data to temp arrays L[] and R[]
     for (i = 0; i < leftCount; i++)
         L[i] = arr[left+i];
     for (j = 0; j < rightCount; j++)
         R[j] = arr[mid+j+1];
 
-    // Merge the temp arrays back into arr[left..right]
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = left; // Initial index of merged subarray
+    i = j = 0;
+    k = left;
     while(i < leftCount && j < rightCount) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
     }
 
-    // Copy the remaining elements of L[], if there are any
-    while(i < leftCount) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
+    while(i < leftCount)
+        arr[k++] = L[i++];
 
-    // Copy the remaining elements of R[], if there are any
-    while(j < rightCount) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+    while(j < rightCount)
+        arr[k++] = R[j++];
 }
 
 void mergeSort(int arr[], int left, int right) {
